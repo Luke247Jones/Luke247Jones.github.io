6c1720fc6af86fb4abc1b7e1315b7e9ef3651f3f
Lucas Jones <66499330+Luke247Jones@users.noreply.github.com>
2021-02-23 16:10:52 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -7,8 +7,13 @@ function fancySchmancy() {
   document.getElementById("textArea").style.fontWeight = "bold";
   document.getElementById("textArea").style.fontFamily = "allegro,verdana,sans-serif";
   document.getElementById("textArea").backgroundColor = "green";
+  alert("Text changed to Fancy Schmancy!");
 }
 
 function boringBetty() {
   document.getElementById("textArea").style.fontStyle = "initial";
+  document.getElementById("textArea").style.fontWeight = "initial";
+  document.getElementById("textArea").style.fontFamily = "initial";
+  document.getElementById("textArea").backgroundColor = "initial";
+  alert("Text changed to Boring Betty..");
 }

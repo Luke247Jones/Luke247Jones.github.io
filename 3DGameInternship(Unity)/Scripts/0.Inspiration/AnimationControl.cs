ea1c2945b03737e24527e716ff7c63394bc9119a
Lucas Jones <66499330+Luke247Jones@users.noreply.github.com>
2022-01-20 18:20:03 -0800

    Add files via upload

---
+++ 3DGameInternship(Unity)/Scripts/0.Inspiration/AnimationControl.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AnimationControl : MonoBehaviour
+{
+    [Header("UI")]
+    public Text animationButtonText;
+    public Text baseText;
+    public Text tText;
+    public Text doubleText;
+    public Text oneDText;
+    public Text twoDText;
+    public Text threeDText;
+    public Text fourDText;
+
+    [Header("Data")]
+    public OrbitCamera orbitCamera;
+    public GameObject[] allUIComponents;
+    public GameObject[] allFurnitureModels;
+    public AnimationClip[] allAnimationClips;
+
+    // Private
+    private int currentIndex = 0;
+    private bool modelUnPacked = true;
+
+    private ModelInfo[] modelsInformation = new ModelInfo[] {
+        new ModelInfo(){ baseNumber = 18, doubleNumber = 0, fourDNumber = 0, oneDNumber = 0, twoDNumber = 4, threeDNumber = 0, tNumber = 0 },
+        new ModelInfo(){ baseNumber = 36, doubleNumber = 0, fourDNumber = 0, oneDNumber = 16, twoDNumber = 0, threeDNumber = 0, tNumber = 0 },
+        new ModelInfo(){ baseNumber = 76, doubleNumber = 142, fourDNumber = 0, oneDNumber = 62, twoDNumber = 0, threeDNumber = 0, tNumber = 72 }
+    };
+
+    private readonly float[] minZooms = new float[] { 2, 2.5f, 3.8f };
+    private readonly string[] buyLinks = new string[] { "https://corkbrick.com/products/evora-coffee-table?_pos=1&_sid=ab70da01f&_ss=r",
+        "https://corkbrick.com/products/coimbra-tv-unit?_pos=1&_sid=e55e1963e&_ss=r",
+        "https://corkbrick.com/products/macau-staircase?_pos=1&_sid=59a211e1a&_ss=r"};
+    private readonly string[] tutorialLinks = new string[] { "https://www.youtube.com/watch?v=G8_kGxzg4Ac",
+        "https://www.youtube.com/watch?v=DXQaL0vC-6M",
+        "https://www.youtube.com/watch?v=J_JBO-m3E9k" };
+
+    private void Awake()
+    {
+        SetInformation();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            currentIndex++;
+            if (currentIndex == allFurnitureModels.Length)
+            {
+                currentIndex = 0;
+            }
+            SetInformation();
+            SwapModels();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            currentIndex--;
+            if (currentIndex == -1)
+            {
+                currentIndex = allFurnitureModels.Length - 1;
+            }
+            SetInformation();
+            SwapModels();
+        }
+    }
+
+    private void SetInformation() {
+        ModelInfo modelInfo = modelsInformation[currentIndex];
+        baseText.text = modelInfo.baseNumber.ToString();
+        tText.text = modelInfo.tNumber.ToString();
+        doubleText.text = modelInfo.doubleNumber.ToString();
+        oneDText.text = modelInfo.oneDNumber.ToString();
+        twoDText.text = modelInfo.twoDNumber.ToString();
+        threeDText.text = modelInfo.threeDNumber.ToString();
+        fourDText.text = modelInfo.fourDNumber.ToString();
+    }
+
+    private void SwapModels()
+    {
+        // Set camera
+        orbitCamera.minDistance = minZooms[currentIndex];
+        orbitCamera.ZoomOut();
+
+        foreach(GameObject model in allFurnitureModels)
+        {
+            model.SetActive(false);
+        }
+        allFurnitureModels[currentIndex].SetActive(true);
+    }
+
+    // Public
+    public void SetIndex(int newIndex) {
+        currentIndex = newIndex;
+        SetInformation();
+        SwapModels();
+    }
+
+    public void PlayAnimation()
+    {
+        Animation animation = allFurnitureModels[currentIndex].GetComponent<Animation>();
+        if (animation.isPlaying) { return; }
+
+        AnimationClip clip = allAnimationClips[currentIndex];
+        animation.clip = clip;
+        animation[clip.name].time = (modelUnPacked) ? clip.length : 0;
+        animation[clip.name].speed = (modelUnPacked) ? -1 : 1;
+        modelUnPacked = !modelUnPacked;
+        animation.Play();
+
+        animationButtonText.text = (modelUnPacked) ? "PACK" : "UNPACK";
+    }
+
+    public void BuyDidTapped()
+    {
+        Application.OpenURL(buyLinks[currentIndex]);
+    }
+
+    public void TutorialDidTapped()
+    {
+        Application.OpenURL(tutorialLinks[currentIndex]);
+    }
+
+    public void BackDidTapped()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void HideDidTapped(Text buttonText)
+    {
+        buttonText.text = (buttonText.text == "HIDE") ? "SHOW" : "HIDE";
+        foreach (GameObject component in allUIComponents)
+        {
+            component.SetActive(buttonText.text == "HIDE");
+        }
+    }
+}
---
+++ 3DGameInternship(Unity)/Scripts/2.Menu/TableView/MainTableController.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+using Assets.Scripts.TableView;
+
+public class MainTableController : MonoBehaviour, ITableViewDataSource, ITableViewDelegate
+{
+    [Header("Main")]
+    public Menu menu;
+    public TableView tableView;
+    public GameObject cellPrefarb;
+
+    [Header("Scenes")]
+    public GameObject noScene;
+
+    [Header("Scene Info")]
+    public GameObject sceneInfoPanel;
+    public Text sceneDescription;
+    public Text challenge;
+
+    [Header("Challenge Info")]
+    public GameObject challengeInfoPanel;
+    public Text challengeDescription;
+    public Image challengeImage;
+
+    // Private
+    private ApiManager apiManager;
+    private ChallengeDTO challengeInformation;
+
+    private void Start()
+    {
+        apiManager = gameObject.AddComponent<ApiManager>();
+
+        // Set table view
+        tableView.Delegate = this;
+        tableView.DataSource = this;
+        tableView.RegisterPrefabForCellReuseIdentifier(cellPrefarb, "ChallengeTableViewCell");
+        SetData(type: 0);
+    }
+
+    /// <summary>
+    /// 0 - Challenges, 1 - Inspiration
+    /// </summary>
+    /// <param name="type"></param>
+    public void SetData(int type)
+    {
+        menu.ShowLoading();
+        switch (type)
+        {
+            case 1:
+                break;
+            default:
+                /*StartCoroutine(apiManager.GetChallenges((response) =>
+                {
+                    menu.HideLoading();
+                    if (response.challenges.Length > 0)
+                    {
+                        GlobalVariables.challengeResponse = response;
+                        GlobalVariables.tableViewDataType = type;
+                        tableView.ReloadData();
+                    }
+                    else
+                    {
+                        SetData(type: type);
+                    }
+                }));*/
+                break;
+        }
+    }
+
+    // Action
+
+    // Challenge Info
+    public void HideChallengeInformation()
+    {
+        challengeInfoPanel.SetActive(false);
+    }
+
+    public void LoadChallenge()
+    {
+        menu.ShowLoading();
+        GlobalVariables.currentChallenge = challengeInformation;
+        PlayerPrefs.SetFloat("length", 1);
+        PlayerPrefs.SetFloat("width", 1);
+        PlayerPrefs.SetInt("challengeType", 3);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("Main");
+    }
+
+    // Scene Info
+
+    public void HideSceneInformation()
+    {
+        sceneInfoPanel.SetActive(false);
+    }
+
+    public void LoadScene()
+    {
+        PlayerPrefs.SetInt("challengeType", 0);
+        SceneManager.LoadScene("Main");
+    }
+
+    // Inspiration
+    public void LoadInspiration()
+    {
+        SceneManager.LoadScene("Inspiration");
+    }
+
+    // Private voids
+
+    private void ShowChallengeInformation()
+    {
+        menu.ShowLoading();
+        challengeDescription.text = challengeInformation.description;
+        StartCoroutine(LoadImage(url: challengeInformation.imageUrl));
+    }
+
+    private IEnumerator LoadImage(string url)
+    {
+        challengeImage.sprite = null;
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            challengeImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+        }
+        challengeInfoPanel.SetActive(true);
+        menu.HideLoading();
+    }
+
+    // Table View Delegates/Data Source
+    public int NumberOfRowsInTableView(TableView tableView)
+    {
+        switch (GlobalVariables.tableViewDataType)
+        {
+            case 0:
+                return GlobalVariables.challengeResponse.challenges.Length;
+            case 1:
+                break;
+            default:
+                return 0;
+        }
+        return 0;
+    }
+
+    public float SizeForRowInTableView(TableView tableView, int row)
+    {
+        return 600f;
+    }
+
+    public TableViewCell CellForRowInTableView(TableView tableView, int row)
+    {
+        TableViewCell cell = tableView.ReusableCellForRow("ChallengeTableViewCell", row);
+        cell.name = "Cell " + row;
+
+        ChallengeTableViewCell challengeTableViewCell = cell.GetComponent<ChallengeTableViewCell>();
+        switch (GlobalVariables.tableViewDataType)
+        {
+            case 0:
+                challengeTableViewCell.SetDefaultImage();
+                ChallengeDTO challenge = GlobalVariables.challengeResponse.challenges[row];
+                StartCoroutine(challengeTableViewCell.LoadImage(challenge.imageUrl));
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
+        return cell;
+    }
+
+    public void TableViewDidHighlightCellForRow(TableView tableView, int row)
+    {
+        // NOT USED
+    }
+
+    public void TableViewDidSelectCellForRow(TableView tableView, int row)
+    {
+        switch (GlobalVariables.tableViewDataType)
+        {
+            case 0:
+                challengeInformation = GlobalVariables.challengeResponse.challenges[row];
+                ShowChallengeInformation();
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
+    }
+}
